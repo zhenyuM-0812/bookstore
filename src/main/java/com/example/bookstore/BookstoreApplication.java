81a60b2b3c2c921c663ee59340b9d6977e3e3a42
Zhenyu Meng <zhenyu.meng.sde@gmail.com>
2026-08-01 03:26:06 -0500

    initialize user service

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
+		SpringApplication.run(
+				BookstoreApplication.class,
+				args);
 	}
 }
